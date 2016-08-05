b9bddb6d05566a90b92911fbd46b5679d36cb6d5
Tony Gaetani <tony.gaetani@gmail.com>
2016-08-05 18:24:00 +0200

    updated public key, links style, added chicago weather and favicon

--- src/cljs/clj_tgcom/content.cljs
+++ src/cljs/clj_tgcom/content.cljs
@@ -21,7 +21,7 @@
   [:span
    [:hr]
    [:a {:class "footer page" :href "#" :on-click (page-change events "weather")} "weather"] [:br]
-   [:p {:class "footer pgp"} "PGP fingerprint 984C EC21 2057 94EF D296  AD3F 436F D09B AD04 F349"]
+   [:p {:class "footer pgp"} "PGP: 984C EC21 2057 94EF D296  AD3F 436F D09B AD04 F349 " [:a {:class "footer page" :href "https://keybase.io/tonygaetani/key.asc" :target "_blank"} "(public key)"]]
    [:p {:class "footer generated-by"} "Fork me on " [:a {:href "https://github.com/tonygaetani/clj-tgcom" :target "_blank"} "github"] "!"]])
 
 (defn index []
@@ -60,18 +60,27 @@
     [:div
      [:iframe {:src "https://free.timeanddate.com/clock/i4mcy0p7/n2020/tlde3/fn13/tt0/th1/ta1"
                :height "18"}]]
-    [:div
+    [:div {:class "weather"}
      [:iframe {:src "https://forecast.io/embed/#lat=48.5200&lon=9.0556&name=Tübingen&units=uk"
                :id "forcast_embed"
                :type "text/html"
                :height "245"
-               :width "50%"}]]
-    [:div
+               :width "100%"}]]
+   [:div
      [:iframe {:src "https://free.timeanddate.com/clock/i4mcy8jh/n43/fn13/tt0/th2/ta1"
                :height "18"}]]
-    [:div
+    [:div {:class "weather"}
      [:iframe {:src "https://forecast.io/embed/#lat=42.3583&lon=-71.0603&name=Boston&units=us"
                :id "forcast_embed"
                :type "text/html"
                :height "245"
-               :width "50%"}]]]])
+               :width "100%"}]]
+    [:div
+     [:iframe {:src "https://freesecure.timeanddate.com/clock/i4mcy8jh/n64/fn13/tt0/th2/ta1"
+               :height "18"}]]
+    [:div {:class "weather"}
+     [:iframe {:src "https://forecast.io/embed/#lat=41.8791&lon=87.6298&name=Chicago&units=us"
+               :id "forcast_embed"
+               :type "text/html"
+               :height "245"
+               :width "100%"}]]]])
