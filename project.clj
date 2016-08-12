0fa8583d4c5c82675e5a9976008e23eb08193952
Tony Gaetani <tony.gaetani@gmail.com>
2016-08-12 09:17:30 +0200

    make site work without js

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject clj-tgcom "0.1.3-SNAPSHOT"
+(defproject clj-tgcom "0.1.4-SNAPSHOT"
   :description "personal website of tony gaetani"
   :url "https://tonygaetani.com"
   :license {:name "Eclipse Public License"
